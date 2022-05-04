c677be469389e32b7b5ddca919e1bc121c737f2a
gxer750 <911gegnaw@gmail.com>
2022-05-04 16:58:31 -0500

    Initial commit

---
+++ src/operators/ArithmeticDemo.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class ArithmeticDemo {
+
+    public static void main (String[] args) {
+
+        int result = 1 + 2;
+        // result is now 3
+        System.out.println("1 + 2 = " + result);
+
+    }
+}
---
+++ src/test/package1/test2.java
@@ -0,0 +1,13 @@
+package test.package1;
+
+public class test2 {
+
+    public static void main(String args []) {
+        boolean a = false;
+        if (a = false)
+            System.out.println("Hello");
+        else
+            System.out.println("Goodbye");
+    }
+
+}
